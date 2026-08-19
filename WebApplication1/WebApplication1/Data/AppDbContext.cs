196510bc61512b56a577d1f06511c094b1f0fa1f
Alice3529 <71872976+Alice3529@users.noreply.github.com>
2026-08-19 16:39:30 +0300

    Remove unnecessary blank line in AppDbContext

--- WebApplication1/WebApplication1/Data/AppDbContext.cs
+++ WebApplication1/WebApplication1/Data/AppDbContext.cs
@@ -5,7 +5,6 @@ namespace WebApplication1.Data
 {
     public class AppDbContext: DbContext
     {
-
         public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
         {
